238a5162798d9f9ab376cc4648c8408d9ad5878e
Jonathan Kurian <kurianj@rose-hulman.edu>
2015-03-24 14:51:52 -0400

    Added Test Four
    Test Four now works

--- PrimeFactors/src/primeFactors/PrimeFactorsTest.java
+++ PrimeFactors/src/primeFactors/PrimeFactorsTest.java
@@ -27,5 +27,9 @@ public class PrimeFactorsTest {
 		assertEquals(list(3), PrimeFactors.generate(3));
 	}
 	
+	@Test
+	public void testFour() throws Exception {
+		assertEquals(list(2,2), PrimeFactors.generate(4));
+	}
 
 }
