d403d5485b9f0c2db74818b024a9debbce12d6d7
Jonathan Kurian <kurianj@rose-hulman.edu>
2015-03-25 23:05:47 -0400

    Added Prime Generator & Test class
    Added Prime Generator & Test class. Created testOne function, which
    returns an empty input

---
+++ PrimeFactors/src/primeFactors/PrimeGenerator.java
@@ -0,0 +1,12 @@
+package primeFactors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimeGenerator {
+	public static List<Integer> generate(int i) {
+		return new ArrayList<Integer>();
+	}
+
+}
---
+++ PrimeFactors/src/primeFactors/PrimeGeneratorTest.java
@@ -0,0 +1,21 @@
+package primeFactors;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeGeneratorTest {
+
+	private List<Integer> list(Integer...integers) {
+		return Arrays.asList(integers);
+	}
+	
+	@Test
+	public void testOne() {
+		assertEquals(list(), PrimeGenerator.generate(1));
+	}
+
+}
